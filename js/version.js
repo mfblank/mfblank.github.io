db09d2bd25ae4d2dcc2d680054f58ff36061562d
BlankMV <42938412+mfblank@users.noreply.github.com>
2023-06-07 10:20:48 +0300

    Update version.js

--- js/version.js
+++ js/version.js
@@ -2,7 +2,7 @@
 if(/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)){
   
 //сам скрипт проверки обновления  
-  if (document.querySelector('#version').textContent !== "0.0.1")
+  if (document.querySelector('#version').textContent !== "0.0.2")
   {
     $(".backupdate").css("display", "block");
     $(".update").css("display", "block");
